72c606f0df6e62ee370a9ba99b42a3e08f3e10e9
zhb <445798850@qq.com>
2017-07-27 23:29:00 +0800

    wechat acsesstoken ready staet

--- ice/server/index.js
+++ ice/server/index.js
@@ -1,34 +1,54 @@
 import Koa from 'koa'
 import Nuxt from 'nuxt'
 
-const app = new Koa()
+import R from 'ramda'
+import {resolve} from 'path'
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
+const MIDDLEWARES=['database','router']
 
-// Start nuxt.js
-async function start () {
-  // Import and Set Nuxt.js options
-  let config = require('../nuxt.config.js')
-  config.dev = !(app.env === 'production')
-  // Instanciate nuxt.js
-  const nuxt = await new Nuxt(config)
-  // Build in development
-  if (config.dev) {
-    try {
-      await nuxt.build()
-    } catch (e) {
-      console.error(e) // eslint-disable-line no-console
-      process.exit(1)
-    }
+let config = require('../nuxt.config.js')
+config.dev = !(process.env === 'production')
+
+const r=path=>resolve(__dirname,path)
+
+class Server {
+  constructor() {
+    this.app = new Koa()
+    this.useMiddleWare(this.app)(MIDDLEWARES) 
   }
+  useMiddleWare(app) {
+    return R.map(R.compose(
+      R.map(i=>i(app)),
+      require,
+      i=>`${r('./middlewares')}/${i}`
+    ))
+  }
+  async start() {
+    // Import and Set Nuxt.js options
 
-  app.use(async (ctx, next) => {
-  ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    await nuxt.render(ctx.req, ctx.res)
-  })
+    // Instanciate nuxt.js
+    const nuxt = await new Nuxt(config)
+    // Build in development
+    if (config.dev) {
+      try {
+        await nuxt.build()
+      } catch (e) {
+        console.error(e) // eslint-disable-line no-console
+        process.exit(1)
+      }
+    }
 
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+    this.app.use(async (ctx, next) => {
+      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+      await nuxt.render(ctx.req, ctx.res)
+    })
+
+    this.app.listen(port, host)
+    console.log('Server listening on ' + host + ':' + port)
+  }
 }
 
-start()
+
+const app=new Server()
+app.start()
