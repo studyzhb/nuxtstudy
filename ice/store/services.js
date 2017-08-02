4bcafbbc2d5154a79c74c4bf900b47cf88aad42c
zhb <445798850@qq.com>
2017-08-02 16:05:45 +0800

    首页结构完成

--- ice/store/services.js
+++ ice/store/services.js
@@ -2,12 +2,23 @@ import axios from 'axios'
 
 const baseUrl=''
 
+const apiUrl='http://rapapi.org/mockjsdata/23570'
+
 class Services{
     getWechatSignature(url){
         return axios.get(`${baseUrl}/wechat-signature?url=${url}`)
     }
     getUserByOAuth(url){
         return axios.get(`${baseUrl}/wechat-oauth?url=${url}`)
+    }
+    fetchHouses(){
+        return axios.get(`${apiUrl}/wiki/houses`)
+    }
+    fetchCities(){
+        return axios.get(`${apiUrl}/wiki/cities`)
+    }
+    fetchCharacters(){
+        return axios.get(`${apiUrl}/wiki/charcters`)
     }
 }
 
